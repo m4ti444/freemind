305f6d7c5afd32af3d65d36d60e34426779e7bc1
m4ti444 <matiasfloresp24@gmail.com>
2025-05-20 23:31:47 -0400

    Delete freemind directory

--- freemind/src/main/java/com/freemind/freemind/repository/TipoActividadRepository.java
+++
@@ -1,9 +0,0 @@
-package com.freemind.freemind.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.freemind.freemind.model.TipoActividad;
-import java.util.List;
-
-public interface TipoActividadRepository extends JpaRepository<TipoActividad, Long> {
-    List<TipoActividad> findByNombre(String nombre);
-}
